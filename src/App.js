0532e455d026d8cef3a1bc6f0d7e134441dbeaa0
rajeshavul2 <rajeshavul2@gmail.com>
2023-11-20 15:21:00 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import Header from './Header';
+import Section from './Section';
+import Article from './Article';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+import './styles.css';
 
-function App() {
+const App = () => {
+  // JavaScript variables for dynamic content
+  const headerTitle = 'My App Header';
+  
+  const section1Heading = 'Section 1';
+  const section1Content = 'This is the content of Section 1.';
+  
+  const section2Heading = 'Section 2';
+  const section2Content = 'This is the content of Section 2.';
+  
+  const article1Title = 'Article 1';
+  const article1Text = 'This is the content of Article 1.';
+  
+  const article2Title = 'Article 2';
+  const article2Text = 'This is the content of Article 2.';
+  
+  const sidebarContent = 'This is the content of the sidebar.';
+  
+  const footerText = '© 2023 Your Company';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title={headerTitle} />
+
+      <Section heading={section1Heading} content={section1Content} />
+      <Section heading={section2Heading} content={section2Content} />
+
+      <Article title={article1Title} text={article1Text} />
+      <Article title={article2Title} text={article2Text} />
+
+      <Sidebar content={sidebarContent} />
+
+      <Footer text={footerText} />
     </div>
   );
-}
+};
 
 export default App;
